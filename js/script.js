bb9dc1d0d57904092ed4fc7faf40d440ba7e4975
stanislavcodes <korchevsky2004@gmail.com>
2020-03-18 09:39:12 +0200

    small changes

--- js/script.js
+++ js/script.js
@@ -1,9 +1,8 @@
 var sections = [];
 function fillArr() {
     for (i = 0; i < 49; i++) {
-        sections[i] = document.querySelector('.section-' + i).className;
-         
-    } 
+        sections[i] = document.querySelector(".section-" + i).className;
+    }
     return sections;
 }
 
@@ -12,18 +11,23 @@ function shuffleArr(array) {
         var j = Math.floor(Math.random() * (i + 1)); // random index from 0 to i
         [array[i], array[j]] = [array[j], array[i]];
     }
+    
 }
-
+    
 function addNewClasses(array) {
-    for(i = 0; i < array.length; i++) {
-        document.querySelector('.section-' + i).classList.remove('.section-' + i);
+    for (i = 0; i < array.length; i++) {
+        document
+            .querySelector(".section-" + i)
+            .classList.remove(".section-" + i);
         //document.querySelector('.section-' + i).classList.add(sections[i]);
-        document.querySelector('.section-' + i).setAttribute('class', sections[i]);
+        document
+            .querySelector(".section-" + i)
+            .setAttribute("class", sections[i]);
     }
 }
 
-document.querySelector('.header').addEventListener('click', function() {
+document.querySelector(".header").addEventListener("click", function() {
     fillArr();
     shuffleArr(sections);
     addNewClasses(sections);
-})
+});
